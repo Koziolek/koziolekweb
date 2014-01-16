30280895af8e1140da578d9c47a0477b271759dc
Koziolek <bjkuczynski@gmail.com>
2014-01-16 21:01:57 +0100

    Template builder test

--- vaadin-clg/src/test/java/pl/koziolekweb/vaadin_clg/CustomLayoutBuilderTest.java
+++ vaadin-clg/src/test/java/pl/koziolekweb/vaadin_clg/CustomLayoutBuilderTest.java
@@ -1,24 +1,66 @@
 package pl.koziolekweb.vaadin_clg;
 
+import org.fest.assertions.Assertions;
+import org.testng.annotations.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+
 /**
  * TODO write JAVADOC!!!
  * User: koziolek
  */
 public class CustomLayoutBuilderTest {
 
-	private static final String TEMPLATE = "package pl.koziolekweb;\n" +
+	public static final String PACKAGE = "pl.koziolekweb";
+	public static final String LAYOUT = "A";
+	public static final String LOCATIONS = "A,B";
+	private static final String TEMPLATE = "package " +
+			PACKAGE +
+			";\n" +
 			"import com.vaadin.ui.Component;\n" +
 			"import com.vaadin.ui.CustomLayout;\n" +
-			"public class A extends CustomLayout {\n" +
+			"public class " +
+			LAYOUT +
+			" extends CustomLayout {\n" +
 			"public static enum Locations{\n" +
-			"A,B\n" +
+			LOCATIONS +
+			"\n" +
 			"}\n" +
-			"private static final String NAME = \"A\";\n" +
-			"public ${class_name}(){" +
+			"private static final String NAME = \"" +
+			LAYOUT +
+			"\";\n" +
+			"public " +
+			LAYOUT +
+			"(){" +
 			"super(NAME);\n" +
 			"}\n" +
 			"public void addComponent(Component c, Locations p) {\n" +
 			"addComponent(c, p.name());\n" +
 			"}\n" +
 			"}";
+
+
+	@Test
+	public void testBuildValidClass() throws Exception {
+		assertThat(CustomLayoutBuilder.init().setPackageName(PACKAGE)
+				.setLayoutName(LAYOUT)
+				.setLocations(LOCATIONS).build()).isEqualTo(TEMPLATE);
+	}
+
+	@Test(expectedExceptions = IllegalStateException.class)
+	public void testBuildNoDataClass() throws Exception {
+		CustomLayoutBuilder.init().build();
+	}
+
+	@Test(expectedExceptions = NullPointerException.class)
+	public void testBuildNullPackage() throws Exception {
+		CustomLayoutBuilder.init().setPackageName(null).build();
+	}
+
+	@Test(expectedExceptions = IllegalArgumentException.class)
+	public void testBuildEmptyPackage() throws Exception {
+		CustomLayoutBuilder.init().setPackageName("").build();
+	}
+
+
 }
