60d95c2b9a914ab34204d2e7326e0a9071795d4d
unknown <bartlomiej.kuczynski@wrodel02.itkontrakt.pl>
2014-03-28 14:49:59 +0100

    after ITK meetup

--- java8/src/main/java/pl/koziolekweb/java8/collections/CollectionsStreams.java
+++ java8/src/main/java/pl/koziolekweb/java8/collections/CollectionsStreams.java
@@ -5,10 +5,7 @@ import com.google.common.collect.Lists;
 import org.testng.annotations.Test;
 import pl.koziolekweb.java8.Range;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static org.fest.assertions.Assertions.assertThat;
@@ -20,50 +17,66 @@ import static org.fest.assertions.Assertions.assertThat;
 public class CollectionsStreams {
 
 
-	public static final int X = 100;
-	public static final int MAX = 100000 * X;
-	public static final int MORE_THAN = 10000 * X;
-	public static final int EXPECTED = 90000 * X;
-	private final ArrayList<Integer> range;
+    public static final int X = 100;
+    public static final int MAX = 100000 * X;
+    public static final int MORE_THAN = 10000 * X;
+    public static final int EXPECTED = 90000 * X;
+    private final ArrayList<Integer> range;
 
-	public CollectionsStreams() {
-		range = Lists.newArrayList(new Range(1, MAX));
-		Collections.shuffle(range);
-	}
+    public CollectionsStreams() {
+        range = Lists.newArrayList(new Range(1, MAX));
+        Collections.shuffle(range);
+    }
 
-	@Test
-	public void simpleTransformStream() {
-		List<Integer> over10000 = range.stream().filter((x) -> x > MORE_THAN).collect(Collectors.toList());
-		assertThat(over10000).hasSize(EXPECTED);
-	}
+    @Test
+    public void testLoop() throws Exception {
+        List<Integer> filtered = new ArrayList<>(MAX);
+        for (int i = 0; i < range.size(); i++) {
+            Integer x = range.get(i);
+            if (x > MORE_THAN && x % 3 == 0)
+                filtered.add(x);
+        }
 
-	@Test
-	public void simpleTransformParallelStream() {
-		List<Integer> over10000 = range.parallelStream().filter((x) -> x > MORE_THAN).collect(Collectors.toList());
-		assertThat(over10000).hasSize(EXPECTED);
-	}
+    }
 
-	@Test
-	public void simpleTransformGuava() {
-		Collection<Integer> over10000 = Collections2.filter(range, (x) -> x > MORE_THAN);
-		assertThat(over10000).hasSize(EXPECTED);
-	}
+    @Test
+    public void simpleTransformStream() {
+        List<Integer> over10000 = range.stream().filter((x) -> x > MORE_THAN).filter(x -> x % 3 == 0)
+                .collect(Collectors.toList());
+//        assertThat(over10000).hasSize(EXPECTED);
+    }
 
-	@Test
-	public void simpleTransformStreamNPE() {
-		List<Integer> over10000 = range.stream().filter((x) -> {
-			if (x == 3) throw new NullPointerException();
-			return x > MORE_THAN;
-		}).collect(Collectors.toList());
-		assertThat(over10000).hasSize(EXPECTED);
-	}
+    @Test
+    public void simpleTransformParallelStream() {
+        List<Integer> over10000 = range.parallelStream()
+                .filter((x) -> x > MORE_THAN)
+                .filter(x -> x % 3 == 0)
+                .collect(Collectors.toList());
+//        assertThat(over10000).hasSize(EXPECTED);
+    }
 
-	@Test
-	public void simpleTransformGuavaNPE() {
-		Collection<Integer> over10000 = Collections2.filter(range, (x) -> {
-			if (x == 3) throw new NullPointerException();
-			return x > MORE_THAN;
-		});
-		assertThat(over10000).hasSize(EXPECTED);
-	}
+    @Test
+    public void simpleTransformGuava() {
+        Collection<Integer> over10000 = Collections2
+                .filter(range, (x) -> x > MORE_THAN);
+        assertThat(over10000).hasSize(EXPECTED);
+    }
+
+    @Test
+    public void simpleTransformStreamNPE() {
+        List<Integer> over10000 = range.stream().filter((x) -> {
+            if (x == 3) throw new NullPointerException();
+            return x > MORE_THAN;
+        }).collect(Collectors.toList());
+        assertThat(over10000).hasSize(EXPECTED);
+    }
+
+    @Test
+    public void simpleTransformGuavaNPE() {
+        Collection<Integer> over10000 = Collections2.filter(range, (x) -> {
+            if (x == 3) throw new NullPointerException();
+            return x > MORE_THAN;
+        });
+        assertThat(over10000).hasSize(EXPECTED);
+    }
 }
--- java8/src/main/java/pl/koziolekweb/java8/date/DateAndTimeBasics.java
+++ java8/src/main/java/pl/koziolekweb/java8/date/DateAndTimeBasics.java
@@ -14,34 +14,34 @@ import java.util.Date;
  */
 public class DateAndTimeBasics {
 
-	@Test
-	public void clock() {
-		Clock defaultZone = Clock.systemDefaultZone();
-		System.err.println(defaultZone);
+    @Test
+    public void clock() {
+        Clock defaultZone = Clock.systemDefaultZone();
+        System.err.println(defaultZone);
 
-		Clock plus2 = Clock.offset(defaultZone, Duration.ofDays(2));
-		System.err.println(plus2);
-	}
+        Clock plus2 = Clock.offset(defaultZone, Duration.ofDays(2));
+        System.err.println(plus2);
+    }
 
-	@Test
-	public void localDate() {
-		LocalDate now = LocalDate.now();
-		System.err.printf("%s-%s-%s\n", now.getDayOfMonth(), now.getMonth(), now.getYear());
+    @Test
+    public void localDate() {
+        LocalDate now = LocalDate.now();
+        System.err.printf("%s-%s-%s\n", now.getDayOfMonth(), now.getMonth(), now.getYear());
 
-		LocalDate localDate = LocalDate.of(2014, 03, 17);
-		System.err.printf("%s-%s-%s\n", localDate.getDayOfMonth(), localDate.getMonth(), localDate.getYear());
+        LocalDate localDate = LocalDate.of(2014, 03, 17);
+        System.err.printf("%s-%s-%s\n", localDate.getDayOfMonth(), localDate.getMonth(), localDate.getYear());
 
-		Date date = new Date(2014, 03, 17);
-		System.err.printf("%s-%s-%s\n", date.getDate(), date.getMonth(), date.getYear());
+        Date date = new Date(2014, 03, 17);
+        System.err.printf("%s-%s-%s\n", date.getDate(), date.getMonth(), date.getYear());
 
-	}
+    }
 
-	@Test
-	public void localTime() {
-		LocalTime localTime = LocalTime.now();
-		System.err.printf("%s:%s\n", localTime.getHour(), localTime.getMinute());
-		LocalTime plus5h = localTime.plusHours(5);
-		System.err.printf("%s:%s\n", plus5h.getHour(), plus5h.getMinute());
+    @Test
+    public void localTime() {
+        LocalTime localTime = LocalTime.now();
+        System.err.printf("%s:%s\n", localTime.getHour(), localTime.getMinute());
+        LocalTime plus5h = localTime.plusHours(5);
+        System.err.printf("%s:%s\n", plus5h.getHour(), plus5h.getMinute());
 
-	}
+    }
 }
