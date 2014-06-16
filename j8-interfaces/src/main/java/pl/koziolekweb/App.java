9961f9fbbea5b5ba3b183e6b1a62138131030b62
Koziolek <bjkuczynski@gmail.com>
2014-06-16 22:13:29 +0200

    http://koziolekweb.pl/2014/06/16/interfejsy-z-implementacja-w-javie-8/

---
+++ j8-interfaces/src/main/java/pl/koziolekweb/App.java
@@ -0,0 +1,46 @@
+package pl.koziolekweb;
+
+import java.util.Collection;
+import java.util.Comparator;
+
+/**
+ * Hello world!
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
+
+interface Sort<T extends Comparable<T>>{
+
+	Collection<T> sort(Collection<T> c);
+
+	default <E> Collection<E> sort(Collection<E> c, Comparator<E> comp ){
+		throw new UnsupportedOperationException("Default impl");
+	};
+}
+
+abstract class AbstractSort<T extends Comparable<T>>{
+
+	abstract Collection<T> sort(Collection<T> c);
+
+}
+
+//interface FreeSort<E> extends Sort{
+//
+//	Collection<E> sort(Collection<E> c, Comparator<E> comp);
+//
+//}
+//
+//class A implements FreeSort<Object>{
+//	@Override
+//	public Collection<Object> sort(Collection<Object> c, Comparator<Object> comp) {
+//		return null;
+//	}
+//
+//	@Override
+//	public Collection sort(Collection c) {
+//		return null;
+//	}
+//}
